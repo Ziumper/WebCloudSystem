68b1465163e92a1a66ca4ff5e0cd03817b35b3aa
tkomoszeski <tomasz.komoszeski@gmail.com>
2019-03-31 12:36:43 +0200

    Disable https redirection

--- WebCloudSystem.Web/Startup.cs
+++ WebCloudSystem.Web/Startup.cs
@@ -43,7 +43,7 @@ namespace WebCloudSystem.Web
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
 
