2e35aefb8448c1d679e12119e1a46233e824548a
tkomoszeski <tomasz.komoszeski@gmail.com>
2019-04-02 23:52:07 +0200

    Implemented user service

--- WebCloudSystem.Bll/Services/Users/UserService.cs
+++ WebCloudSystem.Bll/Services/Users/UserService.cs
@@ -2,20 +2,74 @@ using WebCloudSystem.Bll.Services.Base;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using WebCloudSystem.Bll.Dto.Users;
+using WebCloudSystem.Bll.Helpers;
+using Microsoft.Extensions.Options;
+using WebCloudSystem.Dal.Repositories.Users;
+using WebCloudSystem.Dal.Models;
+using AutoMapper;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Linq;
+using System;
 
 namespace WebCloudSystem.Bll.Services.Users
 {
 
     public class UserService : BaseService, IUserService
     {
+        private List<User> _users = new List<User>
+        { 
+            new User { Id = 1, FirstName = "Test", LastName = "User", Username = "test", Password = "test" } 
+        };
+        private readonly AppSettings _appSettings;
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+
+        public UserService(IOptions<AppSettings> appSettings,IUserRepository userRepository,IMapper mapper) {
+            _appSettings = appSettings.Value;
+            _userRepository = userRepository;
+            _mapper = mapper;
+        }
+
         public UserDtoWithoutPassword Authenticate(string username, string password)
         {
-            throw new System.NotImplementedException();
+             var user = _users.SingleOrDefault(x => x.Username == username && x.Password == password);
+
+            // return null if user not found
+            if (user == null)
+                return null;
+
+            // authentication successful so generate jwt token
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[] 
+                {
+                    new Claim(ClaimTypes.Name, user.Id.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            user.Token = tokenHandler.WriteToken(token);
+
+            // remove password before returning
+            var userWithoutPassword = _mapper.Map<User,UserDtoWithoutPassword>(user);
+
+            return userWithoutPassword;
         }
 
         public IEnumerable<UserDtoWithoutPassword> GetAll()
         {
-            throw new System.NotImplementedException();
+            List<UserDtoWithoutPassword> usersWithoutPassword = new List<UserDtoWithoutPassword>();
+            foreach(var user in _users) {
+                var userDto = _mapper.Map<User,UserDtoWithoutPassword>(user);
+                usersWithoutPassword.Add(userDto);
+            }
+
+            return usersWithoutPassword;
         }
     }
 }
