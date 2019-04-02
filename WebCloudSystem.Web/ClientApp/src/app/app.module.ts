14892302b299cc5b93d068227c2e85a99dc1612a
tkomoszeski <tomasz.komoszeski@gmail.com>
2019-04-03 00:53:01 +0200

    Frontend authentication login and register.

--- WebCloudSystem.Web/ClientApp/src/app/app.module.ts
+++ WebCloudSystem.Web/ClientApp/src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { AlertService } from './services/alert.service';
+import { AuthGuard } from './core/guards/auth.guard';
+import { AuthenticationService } from './services/authentication.service';
+import { UserService } from './services/user.service';
+import { JwtInterceptor } from './core/helpers/jwt.interceptor';
+import { ErrorInterceptor } from './core/helpers/error.interceptor';
 
 @NgModule({
   declarations: [
@@ -28,7 +33,14 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
       { path: 'fetch-data', component: FetchDataComponent },
     ])
   ],
-  providers: [],
+  providers: [
+    AuthGuard,
+    AlertService,
+    AuthenticationService,
+    UserService,
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ WebCloudSystem.Web/ClientApp/src/app/services/user.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../models/User';
+
+
+
+@Injectable()
+export class UserService {
+    private apiUrl: string;
+    constructor(private http: HttpClient) {
+        this.apiUrl = 'api/users';
+    }
+
+    getAll() {
+        return this.http.get<User[]>(this.apiUrl);
+    }
+
+    getById(id: number) {
+        return this.http.get(this.apiUrl + '/' + id);
+    }
+
+    register(user: User) {
+        return this.http.post( this.apiUrl +  '/register', user);
+    }
+
+    update(user: User) {
+        return this.http.put(this.apiUrl + '/' + user.id, user);
+    }
+
+    delete(id: number) {
+        return this.http.delete(this.apiUrl + '/' + id);
+    }
+}
