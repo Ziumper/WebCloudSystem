08051b89d8d9a9d12e5c9d2510933d6603a39019
tkomoszeski <tomasz.komoszeski@gmail.com>
2019-04-05 19:48:30 +0200

    File name on server.

---
+++ WebCloudSystem.Dal/Migrations/20190405174753_FileNameOnServer.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebCloudSystem.Dal.Migrations
+{
+    public partial class FileNameOnServer : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FileNameOnServer",
+                table: "Files",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FileNameOnServer",
+                table: "Files");
+        }
+    }
+}
